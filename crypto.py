11b7990a30ce67df2fb52d43c92b9e812f327197
David Choi <dcchoi2003@gmail.com>
2024-06-06 20:04:06 -0400

    hmac starter code

--- crypto.py
+++ crypto.py
@@ -1,4 +1,6 @@
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.hmac import hashes, HMAC
+import os
 
 
 key = Fernet.generate_key()
@@ -11,6 +13,16 @@ f2 = Fernet(key2)
 
 token = f.encrypt(b"Here is my secret code!")
 
+# HMAC(random key, hash)
+h = HMAC(os.urandom, hashes.SHA256())
+
+h.update(b"message to hash")
+
+h_copy = h.copy() # get a copy of `h' to be reused
+
+h.verify(b"signature")
+
+h_copy.verify(b"an incorrect signature")
 
 print(token)
 
