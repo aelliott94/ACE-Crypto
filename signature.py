fcc7b28c90032d69ca203798acd6dee4a1b00724
David Choi <dcchoi2003@gmail.com>
2024-06-07 19:52:45 -0400

    better signature

--- signature.py
+++ signature.py
@@ -1,55 +1,11 @@
-import hashlib
+from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
 
-from cryptography.hazmat.primitives import hashes
+private_key = Ed25519PrivateKey.generate()
 
-from cryptography.hazmat.primitives.asymmetric import (
-
-   padding, rsa, utils
-
-)
-
-private_key = rsa.generate_private_key(
-
-    public_exponent=65537,
-
-    key_size=2048,
-
-)
-
-prehashed_msg = hashlib.sha256(b"A message I want to sign").digest()
-
-signature = private_key.sign(
-
-    prehashed_msg,
-
-    padding.PSS(
-
-        mgf=padding.MGF1(hashes.SHA256()),
-
-        salt_length=padding.PSS.MAX_LENGTH
-
-    ),
-
-    utils.Prehashed(hashes.SHA256())
-
-)
+signature = private_key.sign(b"my authenticated message")
 
 public_key = private_key.public_key()
 
-public_key.verify(
+# Raises InvalidSignature if verification fails
 
-    signature,
-
-    prehashed_msg,
-
-    padding.PSS(
-
-        mgf=padding.MGF1(hashes.SHA256()),
-
-        salt_length=padding.PSS.MAX_LENGTH
-
-    ),
-
-    utils.Prehashed(hashes.SHA256())
-
-)
+public_key.verify(signature, b"my authenticated message")
