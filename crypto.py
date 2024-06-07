dc094e8e77ea0320bc695d1e9e5535e1326db2ff
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2024-06-06 20:57:20 -0400

    fixed scoping

--- crypto.py
+++ crypto.py
@@ -3,12 +3,12 @@ from cryptography.hazmat.primitives.hmac import hashes, HMAC
 import os
 
 
-def create_hmac(data):
+def create_hmac(data, key):
     h = HMAC(key, hashes.SHA256())
     h.update(data)
     return h.finalize()
 
-def verify_hmac(data, tag):
+def verify_hmac(data, key, tag):
     h = HMAC(key, hashes.SHA256())
     h.update(data)
     h.verify(tag)
@@ -17,13 +17,13 @@ key = Fernet.generate_key()
 f = Fernet(key)
 
 
-token = f.encrypt("Here is my secret code!")
+token = f.encrypt(b"Here is my secret code!")
 
-tag = create_hmac(token)
+tag = create_hmac(token, key)
 
 print("verifying key 1")
 
-verify_hmac(token,tag)
+verify_hmac(token,key, tag)
 
 print("key 1 verified")
 
