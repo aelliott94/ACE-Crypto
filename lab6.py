8ce322d2dd5d9650eed0d3f6445c3a115b41b094
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2024-06-06 20:38:15 -0400

    HMAC up.

---
+++ lab6.py
@@ -0,0 +1,36 @@
+
+
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.hmac import hashes, HMAC
+
+from cryptography.hazmat.primitives import hashes, hmac
+
+
+
+key = b'AAAAAAAAAAAAAAAA'
+
+def create_hmac(data):
+    h = hmac.HMAC(key, hashes.SHA256())
+    h.update(data)
+    return h.finalize()
+
+def verify_hmac(data, tag):
+    h = hmac.HMAC(key, hashes.SHA256())
+    h.update(data)
+    return h.verify(tag)
+
+def main():
+
+    image_bytes = b'asdklfjasdlkfjasldkf'
+    tag = create_hmac(image_bytes)
+
+    print(tag)
+    try:
+        verify_hmac(image_bytes, tag)
+        print("Congratulations")
+    except:
+        print("Failed to verify tag")
+
+
+if __name__ == '__main__':
+    main()
