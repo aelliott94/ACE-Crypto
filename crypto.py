d3b409e5f534fd455fcd196de577579ea7f42487
David Choi <dcchoi2003@gmail.com>
2024-06-07 20:55:28 -0400

    symmetric encryption

--- crypto.py
+++ crypto.py
@@ -11,7 +11,12 @@ def create_hmac(data, key):
 def verify_hmac(data, key, tag):
     h = HMAC(key, hashes.SHA256())
     h.update(data)
-    h.verify(tag)
+
+    try:
+        h.verify(tag)
+        print("HMAC verified!")
+    except:
+        print("HMAC verification failed!")
 
 """
 This encryption scheme uses Fernet, a 128-bit AES scheme in CBC mode.
@@ -21,11 +26,14 @@ def symmetric_encrpytion():
     key = Fernet.generate_key()
     f = Fernet(key)
 
-    token = f.encrypt(b"Here is my secret code!")
+    msg = b"Here is my secret code!"
+    print("Message: ", msg)
+
+    token = f.encrypt(msg)
+    print("Encrypted: ", token)
 
     dec = f.decrypt(token)
-
-    print(dec)
+    print("Decrypted: ", dec)
 
 
 """
@@ -37,14 +45,30 @@ def symmetric_mac():
     # token = f.encrypt(b"Here is my secret code!")
 
     token = b"hello world!"
-    random_key = os.urandom(32)
+    print("Token: ", token)
+    random_key = os.urandom(256)
     tag = create_hmac(token, random_key)
+    print("HMAC: ", tag)
     verify_hmac(token,random_key, tag)
 
 
 def main():
+
+    print("Starting symmetric encryption algorithm:")
     symmetric_encrpytion()
+
+    print()
+
+    print("Starting symmetric MAC:")
     symmetric_mac()
+
+    print()
+
+    print("Starting asymmetric key exchange:")
+
+    print()
+
+    print("Starting asymmetric signature:")
 
 if __name__ == '__main__':
     main()
